f2a419dcca78d865f82b8308341cf1e855cbc156
Coen Hallie <me@coenhallie.com>
2021-11-17 16:39:02 +0000

    add conditional button and small styling change

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex justify-between px-8 py-4 dark:bg-gray-700 bg-gray-100 border-b border-gray-300 dark:border-gray-500">
-    <button v-if="!main.darkMode" class="bg-red-500 rounded-lg p-2 text-white font-medium" @click="toggleMode()"> toggle Dark </button>
-    <button v-if="main.darkMode" class="bg-blue-500 rounded-lg p-2 text-white font-medium" @click="toggleMode()"> toggle Light </button>
-    <button class="text-left bg-red-500 rounded-lg p-2 text-white font-medium">{{main.accountAddress}}</button>
+    <button v-if="!main.darkMode" class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-lg p-2 text-white font-medium" @click="toggleMode()"> toggle Dark </button>
+    <button v-if="main.darkMode" class="bg-gradient-to-r from-blue-500 to-green-400 hover:from-pink-500 hover:to-yellow-500 rounded-lg p-2 text-white font-medium" @click="toggleMode()"> toggle Light </button>
+    <button class="text-left bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-lg p-2 text-white font-medium">{{main.accountAddress}}</button>
   </div>
   <div class="min-h-screen bg-gray-100 dark:bg-gray-700">
     <router-view/>
--- src/components/TokenSwapContainer.vue
+++ src/components/TokenSwapContainer.vue
@@ -3,7 +3,6 @@
     <div class="shadow-md min-w-min w-3/12 rounded-3xl py-4 bg-white dark:bg-gray-800">
       <div class="flex justify-between">
         <h3 class="p-4 text-2xl font-medium dark:text-white">Swap</h3>
-        <h3 class="p-4 text-xs"><img :src="settingsGear" alt="settings-logo" /></h3>
       </div>
       <div class="m-2 p-2 border-solid border border-gray-100 dark:border-opacity-10 rounded-3xl">
         <h3 class="font-semibold text-left dark:text-white">From</h3>
@@ -11,13 +10,13 @@
           <input type="number" placeholder="0.0" v-model="toTokenAmount" min="0"
           class="form-input px-4 py-1 text-3xl border-0 focus:outline-none dark:text-white dark:bg-gray-800">
           <button v-if="main.fromToken === ''" @click="main.openTokenModal('from')" class="px-3 w-42 text-white flex items-center font-semibold
-          rounded-3xl bg-blue-500 dark:text-white"> Select token
+          rounded-3xl bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 dark:text-white"> Select token
           <svg width="17" height="7" fill="none">
             <path d="M.972 1l5.233 5 5.234-5" stroke="#fff"></path>
           </svg>
           </button>
           <button v-if="main.fromToken !== ''" @click="main.openTokenModal('from')" class="px-3 w-42 text-white flex items-center font-semibold
-          rounded-3xl bg-blue-500 dark:text-white">
+          rounded-3xl bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 dark:text-white">
           <img :src="`icons/${main.fromToken.token}.png`" :alt="main.fromToken.token" class="w-7 h-7 mr-3">
           {{main.fromToken.token}}
           <svg width="17" height="7" fill="none">
@@ -35,13 +34,13 @@
           <p v-if="main.poolTokenInfo" class="text-3xl dark:text-white py-1">{{ calculateAmount }}</p>
           <p v-if="!main.poolTokenInfo" class="text-3xl dark:text-white py-1">0</p>
           <button v-if="main.toToken === ''" @click="main.openTokenModal('to')" class="px-3 w-42 text-white flex items-center font-semibold
-          rounded-3xl bg-blue-500"> Select token
+          rounded-3xl bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500"> Select token
           <svg width="17" height="7" fill="none">
             <path d="M.972 1l5.233 5 5.234-5" stroke="#fff"></path>
           </svg>
           </button>
           <button v-if="main.toToken !== ''" @click="main.openTokenModal('to')" class="px-3 w-42 text-white flex items-center font-semibold
-          rounded-3xl bg-blue-500 dark:text-white">
+          rounded-3xl bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 dark:text-white">
           <img :src="`icons/${main.toToken.token}.png`" :alt="main.toToken.token" class="w-7 h-7 mr-3">
            {{main.toToken.token}}
           <svg width="17" height="7" fill="none">
@@ -52,12 +51,8 @@
       </div>
       <token-selection-modal v-if="main.showTokenModal" />
       <div class="m-4">
-        <button v-if="toToken && main.poolTokenInfo" class="px-3 py-4 bg-blue-200 dark:bg-blue-400 w-full rounded-3xl
-         text-2xl text-blue-600 text-opacity-50 font-semibold dark:text-white"> swap</button>
-         <button disabled v-if="!toToken" class="px-3 py-4 bg-blue-200 w-full rounded-3xl
-         text-2xl text-blue-600 text-opacity-50 font-semibold dark:text-white dark:bg-blue-400"> Select token</button>
-         <button disabled v-if="!main.poolTokenInfo && toToken" class="px-3 py-4 bg-blue-200 w-full rounded-3xl
-         text-lg text-blue-600 text-opacity-50 font-semibold dark:text-white dark:bg-blue-400"> Insufficient liquidity for this trade.</button>
+         <button disabled class="px-3 py-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 w-full rounded-3xl
+         text-lg text-white font-semibold">{{buttonText}}</button>
       </div>
       <div v-if="main.poolTokenPrice"> {{main.poolTokenInfo}} per 1 {{main.toToken}} </div>
     </div>
@@ -88,7 +83,7 @@ export default {
     const main = useTokenStore();
     const toTokenAmount = ref(0);
     const {
-      openTokenModal, showTokenModal, tokens, toToken, fromToken, filterTokens, fetchPools, pools, poolTokenInfo,
+      openTokenModal, showTokenModal, tokens, toToken, fromToken, filterTokens, fetchPools, pools, poolTokenInfo, balances,
     } = storeToRefs(main);
     main.fetchPools();
 
@@ -97,6 +92,22 @@ export default {
     });
 
     const calculateAmount = computed(() => Math.round((toTokenAmount.value / main.poolTokenInfo.price) * 100) / 100);
+
+    const buttonText = computed(() => {
+      if (toTokenAmount.value > main.fromToken.balance) {
+        return `Not enough ${main.fromToken.token}`;
+      }
+      if (!toToken.value && !fromToken.value) {
+        return 'Select token';
+      }
+      if (!main.poolTokenInfo && toToken) {
+        return 'Insufficient liquidity for this trade.';
+      }
+      if (toToken && main.poolTokenInfo) {
+        return 'Swap';
+      }
+      return 'Swap';
+    });
 
     return {
       settingsGear,
@@ -109,7 +120,9 @@ export default {
       tokens,
       toToken,
       pools,
+      balances,
       fromToken,
+      buttonText,
       filterTokens,
       fetchPools,
       TokenPoolDetails,
